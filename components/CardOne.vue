3611a5f378ed7d310073c7b275c92a6cf815f074
pamtel <pamelaakosahiante@gmail.com>
2021-12-11 13:03:13 +0100

    first commit

---
+++ components/CardOne.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="Hero">
+    <div class="text-wrapper">
+      <p class="header">Travling!</p>
+      <!-- <button class="button btn">Start Looking</button> -->
+    </div>
+    <img
+      src="~assets/images/SideRight.png"
+      alt="right-side-image"
+      class="image"
+    />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.Hero {
+  max-height: 100vh;
+  max-width: 1200px;
+  overflow: hidden;
+  position: relative;
+  margin: 20px 0;
+  padding: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background-color: #ffff;
+  border-radius: 10px;
+}
+.text-wrapper {
+  width: 50%;
+  background-color: #ffff;
+  padding: 0 20px;
+}
+.header {
+  font-weight: 600;
+  font-size: 28px;
+  background-color: #ffff;
+  color: #42a7c3;
+}
+.image {
+  width: 27.5rem;
+  border-radius: 10px;
+}
+.button {
+  padding: 1rem 4rem;
+  border-radius: 100rem;
+  background-color: rgb(231, 81, 43);
+  color: white;
+  width: 20rem;
+  font-weight: 700;
+  transition: 0.5s;
+}
+.button:hover {
+  width: 21.5rem;
+}
+@media (max-width: 768px) {
+  .Hero {
+    height: auto;
+  }
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template class="layout">
+    <div class="container">
+      <CardOne />
+    </div>
+</template>
+
+<script>
+
+export default {};
+</script>
+
+<style scoped>
+  .layout {
+    background-color: #e5e5e5;
+  }
+</style>
